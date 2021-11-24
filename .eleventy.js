c635eb00271428945ac88878ad8ef1687380d38b
Michael Stewart <mikrostew@gmail.com>
2021-11-24 13:31:21 -0500

    setup basic config and favicon

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+// see https://www.11ty.dev/docs/config/
+module.exports = function (eleventyConfig) {
+  // make sure favicon is copied over
+  eleventyConfig.addPassthroughCopy('src/favicon.ico');
+
+  // other config that doesn't use the API
+  return {
+    dir: {
+      // this builds src/ --> _site/
+      input: 'src',
+      output: '_site',
+      // these 3 are relative to dir.input
+      includes: '_includes',
+      layouts: '_layouts',
+      data: '_data',
+      // set default template engines
+      dataTemplateEngine: 'njk',
+      markdownTemplateEngine: 'njk',
+      htmlTemplateEngine: 'njk',
+    },
+  };
+};
